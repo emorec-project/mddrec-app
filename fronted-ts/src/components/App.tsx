ad5329fc10f52a628e2639768233e9b6adafbbe6
Almog Tavor <almogtavor@gmail.com>
2023-07-29 19:35:38 +0300

    feat: split to files

--- fronted-ts/src/components/App.tsx
+++ fronted-ts/src/components/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
-import { Button, Upload, message, Alert, Collapse } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import { Alert } from 'antd';
+import { RecordedSessions } from './RecordedSessions';
+import { Record } from './Record';
+import { UploadFiles } from './Upload';
 
 const App: React.FC = () => {
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
@@ -12,9 +14,9 @@ const App: React.FC = () => {
   const [timer, setTimer] = useState(0);
   const [error, setError] = useState<string | null>(null);
   const [sessions, setSessions] = useState<string[]>([]);
-  const [isCollapsibleOpen, setIsCollapsibleOpen] = useState(false);
 
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+  const [uploadedAudio, setUploadedAudio] = useState<File | null>(null);
 
   const handleDataAvailable = (e: BlobEvent) => {
     setRecordedChunks((prev) => prev.concat(e.data));
@@ -56,12 +58,6 @@ const App: React.FC = () => {
     }
   }, [capturing]);
 
-  useEffect(() => {
-    if (videoRef.current && mediaStream) {
-      videoRef.current.srcObject = mediaStream;
-    }
-  }, [mediaStream]);
-
   const handleStopRecording = () => {
     if (mediaRecorderRef.current && recordedChunks.length) {
       const blob = new Blob(recordedChunks, { type: recordingAudio ? 'audio/webm' : 'video/webm' });
@@ -83,10 +79,7 @@ const App: React.FC = () => {
     return () => clearInterval(interval);
   }, [capturing]);
 
-  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
-  const [uploadedAudio, setUploadedAudio] = useState<File | null>(null);
-
-  const handleUpload = (file: any) => {
+  const handleUpload = (file: any): false => {
     const fileType = file.type.split('/')[0];
   
     if (fileType === 'audio') {
@@ -94,10 +87,10 @@ const App: React.FC = () => {
     } else if (fileType === 'video') {
       setUploadedFile(file);
     }
-
+  
     return false;
   }
-
+  
 
   useEffect(() => {
     if (uploadedFile) {
@@ -123,75 +116,20 @@ const App: React.FC = () => {
     setMediaStream(null);
   }
 
-  const toggleCollapsible = () => {
-    setIsCollapsibleOpen(!isCollapsibleOpen);
-  }
-
   return (
     <div>
       {error && <Alert message={`Error: ${error}`} type="error" />}
-
-      {mediaStream && !recordingAudio && <video ref={videoRef} autoPlay muted></video>}
-
-      {capturing && recordingAudio && <Alert message="Recording..." type="info" />}
-
-      <Upload
-      accept="video/mp4"
-      beforeUpload={handleUpload}
-      showUploadList={false}
-    >
-      <Button icon={<UploadOutlined />}>Upload an MP4 file</Button>
-    </Upload>
-
-    <Upload
-      accept="audio/mp3"
-      beforeUpload={handleUpload}
-      showUploadList={false}
-    >
-      <Button icon={<UploadOutlined />}>Upload an MP3 file</Button>
-    </Upload>
-      <Button 
-        style={{fontSize: '3rem', backgroundColor: capturing ? 'red' : 'green'}}
-        onClick={() => setCapturing(!capturing)}
-      >
-        {capturing ? 'Stop Recording' : 'Start Recording'}
-      </Button>
-
-      <Button 
-        style={{fontSize: '2rem'}}
-        onClick={toggleRecordingMode}
-      >
-        {recordingAudio ? 'Switch to Video' : 'Switch to Audio'}
-      </Button>
-
-      <Alert
-        type="info"
-        message={recordingAudio ? 'Currently in Audio Mode' : 'Currently in Video Mode'}
+      <Record 
+        capturing={capturing}
+        setCapturing={setCapturing}
+        recordingAudio={recordingAudio}
+        timer={timer}
+        toggleRecordingMode={toggleRecordingMode}
+        mediaStream={mediaStream}
       />
-
-      <Alert
-        type={capturing ? "success" : "warning"}
-        message={capturing ? 'Currently Recording' : 'Currently Not Recording'}
-      />
-
-      <p>Recording Time: {timer} seconds</p>
-
-      <div className='sessionsContainer'>
-        <Collapse>
-          <Collapse.Panel header={`Recorded Sessions (${sessions.length})`} key="1">
-            {sessions.map((session, index) => (
-              <div key={index}>
-                {recordingAudio ? 
-                  <audio src={session} controls></audio> : 
-                  <video src={session} controls></video>
-                }
-              </div>
-            ))}
-          </Collapse.Panel>
-        </Collapse>
-      </div>
+      <UploadFiles handleUpload={handleUpload} />
+      <RecordedSessions sessions={sessions} recordingAudio={recordingAudio} />
     </div>
-
   );
 };
 
---
+++ fronted-ts/src/components/RecordedSessions.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Collapse } from 'antd';
+
+interface RecordedSessionsProps {
+  sessions: string[];
+  recordingAudio: boolean;
+}
+
+export const RecordedSessions: React.FC<RecordedSessionsProps> = ({ sessions, recordingAudio }) => {
+  return (
+    <div className='sessionsContainer'>
+      <Collapse>
+        <Collapse.Panel header={`Recorded Sessions (${sessions.length})`} key="1">
+          {sessions.map((session, index) => (
+            <div key={index}>
+              {recordingAudio ? 
+                <audio src={session} controls></audio> : 
+                <video src={session} controls></video>
+              }
+            </div>
+          ))}
+        </Collapse.Panel>
+      </Collapse>
+    </div>
+  );
+};
