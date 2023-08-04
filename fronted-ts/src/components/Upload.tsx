8579ada9f8a89be573bb9dd77af0a9d95d17b1eb
Almog Tavor <70065337+almogtavor@users.noreply.github.com>
2023-08-04 17:57:20 +0300

    feat: migrate the upload logic to work in chunks

--- fronted-ts/src/components/Upload.tsx
+++ fronted-ts/src/components/Upload.tsx
@@ -12,20 +12,32 @@ export const UploadFiles: React.FC<UploadFilesProps> = ({ addSession }) => {
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
 
   const uploadFile = (file: File) => {
-    let formData = new FormData();
-    formData.append('file', file);
-
-    axios.post('http://localhost:8000/blobs_manager/upload/', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-      withCredentials: true
-    }).then(response => {
-      console.log(response);
-    }).catch(error => {
-      console.error(error);
-    });
-  }
+    const CHUNK_SIZE = 1024 * 1024; // 1MB
+    const fileSize = file.size;
+    const chunksCount = Math.ceil(fileSize / CHUNK_SIZE);
+  
+    for (let i = 0; i < chunksCount; i++) {
+      let start = CHUNK_SIZE * i;
+      let end = CHUNK_SIZE * (i + 1);
+      let chunk = file.slice(start, end);
+  
+      let formData = new FormData();
+      formData.append('file', chunk);
+      formData.append('filename', file.name);
+      formData.append('chunkIndex', i.toString());
+      formData.append('chunksCount', chunksCount.toString());
+  
+      axios.post('http://localhost:8000/blobs_manager/upload/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then(response => {
+        console.log(response);
+      }).catch(error => {
+        console.error(error);
+      });
+    }
+  } 
 
   const handleUpload = (file: any): false => {
     const fileType = file.type.split('/')[0];
