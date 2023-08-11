d12b78bf247222a0136e5cb30fa341fbc6f407dc
Almog Tavor <almogtavor@gmail.com>
2023-08-11 18:30:33 +0300

    feat: improve code split to react & app.tsx files

--- fronted-ts/src/components/App.tsx
+++ fronted-ts/src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Alert } from 'antd';
 import { RecordedSessions } from './RecordedSessions';
@@ -6,102 +6,26 @@ import { Record } from './Record';
 import { UploadFiles } from './Upload';
 
 const App: React.FC = () => {
-  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const [capturing, setCapturing] = useState(false);
   const [recordingAudio, setRecordingAudio] = useState(false);
-  const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
-  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
-  const [timer, setTimer] = useState(0);
-  const [error, setError] = useState<string | null>(null);
   const [sessions, setSessions] = useState<string[]>([]);
-
-  const handleDataAvailable = (e: BlobEvent) => {
-    setRecordedChunks((prev) => prev.concat(e.data));
-  }
-
-  const stopRecording = () => {
-    if (mediaRecorderRef.current) {
-      mediaRecorderRef.current.stop();
-    }
-  }
-
-  const startMediaRecorder = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: recordingAudio, video: !recordingAudio });
-      setMediaStream(stream);
-      const mediaRecorder = new MediaRecorder(stream, { mimeType: recordingAudio ? 'audio/webm' : 'video/webm' });
-      mediaRecorder.ondataavailable = handleDataAvailable;
-      mediaRecorder.onstop = handleStopRecording;
-      mediaRecorderRef.current = mediaRecorder;
-
-      if (capturing) {
-        mediaRecorderRef.current.start(5000);
-      }
-    } catch (err) {
-      setError(`Error accessing media devices: ${(err as Error).message}`);
-    }
-  };
-
-  useEffect(() => {
-    if(capturing) {
-      startMediaRecorder();
-    } else if(mediaRecorderRef.current) {
-      stopRecording();
-    }
-  
-    return () => {
-      mediaStream?.getTracks().forEach((track) => track.stop());
-      setMediaStream(null);
-    }
-  }, [capturing]);
-
-  const handleStopRecording = () => {
-    if (mediaRecorderRef.current && recordedChunks.length) {
-      const blob = new Blob(recordedChunks, { type: recordingAudio ? 'audio/webm' : 'video/webm' });
-      const url = URL.createObjectURL(blob);
-      setSessions(prev => [...prev, url]);
-      setRecordedChunks([]);
-    }
-  }
-
-  useEffect(() => {
-    let interval: NodeJS.Timeout;
-    if (capturing) {
-      interval = setInterval(() => {
-        setTimer((prevTimer) => prevTimer + 1);
-      }, 1000);
-    } else {
-      setTimer(0);
-    }
-    return () => clearInterval(interval);
-  }, [capturing]);  
 
   const addSession = (url: string) => {
     setSessions(prev => [...prev, url]);
   };
 
   const toggleRecordingMode = () => {
-    if (capturing) {
-      stopRecording();
-      setCapturing(false);
-    }
     setRecordingAudio(!recordingAudio);
-    if (mediaStream) {
-      mediaStream.getTracks().forEach((track) => track.stop());
-    }
-    setMediaStream(null);
   }
 
   return (
     <div>
-      {error && <Alert message={`Error: ${error}`} type="error" />}
       <Record 
         capturing={capturing}
         setCapturing={setCapturing}
         recordingAudio={recordingAudio}
-        timer={timer}
         toggleRecordingMode={toggleRecordingMode}
-        mediaStream={mediaStream}
+        addSession={addSession}
       />
       <UploadFiles 
         addSession={addSession}
--- fronted-ts/src/components/Record.tsx
+++ fronted-ts/src/components/Record.tsx
@@ -6,13 +6,16 @@ interface RecordProps {
   capturing: boolean;
   setCapturing: (capturing: boolean) => void;
   recordingAudio: boolean;
-  timer: number;
   toggleRecordingMode: () => void;
-  mediaStream: MediaStream | null;
+  addSession: (url: string) => void;
 }
 
-export const Record: React.FC<RecordProps> = ({ capturing, setCapturing, recordingAudio, timer, toggleRecordingMode, mediaStream }) => {
+export const Record: React.FC<RecordProps> = ({ capturing, setCapturing, recordingAudio, toggleRecordingMode, addSession }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [timer, setTimer] = useState(0);
 
   useEffect(() => {
     if (videoRef.current && mediaStream) {
@@ -20,39 +23,94 @@ export const Record: React.FC<RecordProps> = ({ capturing, setCapturing, recordi
     }
   }, [mediaStream]);
 
+  const handleDataAvailable = (e: BlobEvent) => {
+    setRecordedChunks((prev) => prev.concat(e.data));
+  }
+
+  const handleStopRecording = () => {
+    if (mediaRecorderRef.current && recordedChunks.length) {
+      const blob = new Blob(recordedChunks, { type: recordingAudio ? 'audio/webm' : 'video/webm' });
+      const url = URL.createObjectURL(blob);
+      addSession(url);
+      setRecordedChunks([]);
+    }
+  }
+
+  const stopRecording = () => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+    }
+  }
+
+  const startMediaRecorder = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: recordingAudio, video: !recordingAudio });
+      setMediaStream(stream);
+      const mediaRecorder = new MediaRecorder(stream, { mimeType: recordingAudio ? 'audio/webm' : 'video/webm' });
+      mediaRecorder.ondataavailable = handleDataAvailable;
+      mediaRecorder.onstop = handleStopRecording;
+      mediaRecorderRef.current = mediaRecorder;
+      
+      if (capturing) {
+        mediaRecorderRef.current.start(5000);
+      }
+    } catch (err: any) {
+      console.error("Error accessing media devices:", err.message);
+    }
+  };
+
+  useEffect(() => {
+    if(capturing) {
+      startMediaRecorder();
+    } else if(mediaRecorderRef.current) {
+      stopRecording();
+    }
+  
+    return () => {
+      mediaStream?.getTracks().forEach((track) => track.stop());
+      setMediaStream(null);
+    }
+  }, [capturing]);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (capturing) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+    } else {
+      setTimer(0);
+    }
+    return () => clearInterval(interval);
+  }, [capturing]);
+
   return (
     <div>
       {mediaStream && !recordingAudio && <video className={"video"} ref={videoRef} autoPlay muted></video>}
-
       {capturing && recordingAudio && <Alert className={"alert"} message="Recording..." type="info" />}
-
       <Button 
-      type="primary"
+        type="primary"
         className={capturing ? "stopRecordingButton" : "startRecordingButton"}
         onClick={() => setCapturing(!capturing)}
       >
         {capturing ? 'Stop Recording' : 'Start Recording'}
       </Button>
-
       <Button 
         className={"toggleRecordingModeButton"}
         onClick={toggleRecordingMode}
       >
         {recordingAudio ? 'Switch to Video' : 'Switch to Audio'}
       </Button>
-
       <Alert
         className={"infoAlert"}
         type="info"
         message={recordingAudio ? 'Currently in Audio Mode' : 'Currently in Video Mode'}
       />
-
       <Alert
         className={capturing ? "successAlert" : "warningAlert"}
         type={capturing ? "success" : "warning"}
         message={capturing ? 'Currently Recording' : 'Currently Not Recording'}
       />
-
       <p className={"recordingTime"}>Recording Time: {timer} seconds</p>
     </div>
   );
