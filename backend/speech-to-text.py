fa29d609411bb6a9e6423ebec6f18eec00e32ea3
kfirlevi <86306621+kfirlevi@users.noreply.github.com>
2023-08-26 14:26:26 +0300

    adding func to stt

--- backend/speech-to-text.py
+++ backend/speech-to-text.py
@@ -2,6 +2,13 @@ import whisper
 
 model = whisper.load_model('base')
 path = r'C:\Users\Shira\Documents\Hackathon 2023\code\AVEC_patient_300_self_recording_for_test.wav'
-# path = r'uploads\AVEC_patient_300_self_recording_for_test.wav'
-result = model.transcribe(path, fp16=False)
-print(result)
+rel_path = r'backend\uploads\AVEC_patient_300_self_recording_for_test.wav'
+# result = model.transcribe(rel_path, fp16=False)
+# print(result['text'])
+
+def get_stt_from_path(path):
+    result = model.transcribe(path, fp16=False)
+    return result['text']
+
+stt = get_stt_from_path(rel_path)
+print(stt)
