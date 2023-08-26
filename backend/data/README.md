c9e40eab5ed2c4484f9f47274500240021a13aee
Noam-Bialik <n.o.bialik@gmail.com>
2023-08-26 03:43:51 +0300

    feat: mongo readme

---
+++ backend/data/README.md
@@ -0,0 +1,4 @@
+# Mongo
+**important**: if you run the server local, you must include your external ip to connect to mongodb. 
+u should go to Atlas mongodb web, sign in with noams credentials, and u supuse to find a bottun that will add your ip.
+![alt text](add_ip.png)
--- backend/data/mongo.py
+++ backend/data/mongo.py
@@ -1,12 +1,15 @@
 import pymongo
 from dtos import Document
 
+MONGO_ID = "_id"
+DOCUMENT_ID = "id"
+
 myclient = pymongo.MongoClient("mongodb+srv://mdd-access:mdd-read-write@learn0.fu403zg.mongodb.net/")
 mydb = myclient["learn0"]
 mycol = mydb["sample"]
 
 def find_doc_by_id(_id: str):
-    result = mycol.find_one({"_id": _id})
+    result = mycol.find_one({MONGO_ID: _id})
     return result
 
 def insert_doc(doc: Document):
@@ -24,5 +27,5 @@ def insert_docs(docs : list[Document]):
 
 def create_mongo_doc(doc: Document):
     mongoDoc = dict(doc)
-    mongoDoc["_id"] = mongoDoc["id"]
+    mongoDoc[MONGO_ID] = mongoDoc[DOCUMENT_ID]
     return mongoDoc
