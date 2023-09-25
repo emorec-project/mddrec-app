f86a58f2a6b156a160bdd48d0101c1f398d83196
Noam-Bialik <n.o.bialik@gmail.com>
2023-09-25 19:32:44 +0300

    feature: run with docker

--- backend/config_loader.py
+++ backend/config_loader.py
@@ -9,3 +9,5 @@ if profile == 'prod':
     from config.config_prod import *
 elif profile == 'test':
     from config.config_test import *
+elif profile == 'remote-mongo':
+    from config.config_test_remote_mongo import *
